11a3d7606176a3682eeb4018e005245fe3d3fdfb
Dennis Brinkman <dennisbrinkman93@gmail.com>
2018-04-15 21:56:51 +0200

    Make 32-bit compatible and add more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,11 +30,19 @@ StandardSystem = PlanetarySystem(ListOfBodies)
 StandardPos = [6378.0, 0.0, 0.0]
 scpos = GetInertialFramePos(StandardSystem, Earth, StandardPos, ep)
 
+#Another StandardSystem
+ListOfBodies2 = [Sun,Earth,Moon] 
+StandardSystem2 = PlanetarySystem(ListOfBodies2)
+scpos2 = GetInertialFramePos(StandardSystem2, Earth, StandardPos, ep)
+
 #Perform tests
 @testset "Environment Tests" begin 
     @test FlightSimulator.parent(Earth) == FlightSimulator.parent(Moon)
     @test round.(Earth.Environment.GravityModel(Earth, [6378.0, 0.0, 0.0])*100000000)/100000 == [0.0, 0.0, 9.79871]
     @test PlanetarySystem(ListOfBodies) == StandardSystem
+    @test PlanetarySystem(ListOfBodies2) == StandardSystem2
     @test StandardSystem.InertialFrame_Body == EarthMoonBarycenter
+    @test StandardSystem2.InertialFrame_Body == SSB 
     @test round.(GetGravAccel(StandardSystem, scpos, ep)*100000000)/100000 == [-1.72622, -9.64544, -1.0e-5]
+    @test round.(GetGravAccel(StandardSystem2, scpos2, ep)*100000000)/100000 == [-1.76066, -9.47293, 0.06882]
 end
